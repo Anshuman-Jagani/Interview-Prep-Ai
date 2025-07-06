d8962d0529df5c4be99fdb2d64ba88a8d3d82be8
Anshuman Jagani <anshuman.r.1797@gmail.com>
2025-07-06 17:15:12 +0530

    Auth backend Completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-require("dotenv").congig();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -14,10 +16,18 @@ app.use(
     })
 );
 
+connectDB()
+
 // Middleware
 app.use(express.json());
 
 //Routes
+app.use("/api/auth", authRoutes);
+//app.use("/api/sessions", sessionRoutes);
+//app.use("/api/questions", questionRoutes);
+
+//app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+//app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 //Server Uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
