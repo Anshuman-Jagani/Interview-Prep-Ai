4b2823669bcf907d3f157372d4125365dc1da588
Anshuman Jagani <anshuman.r.1797@gmail.com>
2025-07-06 22:54:07 +0530

    Gemini Integration Complete

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
 const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 
 const app = express();
 
