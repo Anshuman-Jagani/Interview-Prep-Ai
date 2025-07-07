7dcb67f8482c98c541f1c055932441a5e95619f3
Anshuman Jagani <anshuman.r.1797@gmail.com>
2025-07-07 23:31:38 +0530

    AI Response Answer

--- frontend/interview-prep-ai/src/components/Cards/QusetionCard.jsx
+++ frontend/interview-prep-ai/src/components/Cards/QusetionCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { LuChevronDown, LuPin, LuPinOff, LuSparkles } from 'react-icons/lu';
+import AIResponsePreview from '../../pages/InterviewPrep/components/AIResponsePreview';
 
 const QusetionCard = ({
     question,
